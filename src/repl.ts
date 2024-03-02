b830fbab780b7ed4a6107a8d04b31c275cd10c82
tommy <tommiful999@gmail.com>
2024-03-02 15:21:47 +0800

    fix rep

--- src/repl.ts
+++ src/repl.ts
@@ -3,9 +3,6 @@ import { repl } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
-/**
- * https://docs.nestjs.com/recipes/repl
- */
 async function bootstrap(): Promise<void> {
   await repl(AppModule);
 }
