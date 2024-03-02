494f629649e7e6f66921380877cf7ed5c98fc697
tommy <tommiful999@gmail.com>
2024-03-02 15:06:06 +0800

    remove unnecessary files

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,6 @@ import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 import { middleware } from './app.middleware';
 import { AppModule } from './app.module';
 
-/**
- * https://docs.nestjs.com
- * https://github.com/nestjs/nest/tree/master/sample
- * https://github.com/nestjs/nest/issues/2249#issuecomment-494734673
- */
 async function bootstrap(): Promise<string> {
   const isProduction = process.env.NODE_ENV === 'production';
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
