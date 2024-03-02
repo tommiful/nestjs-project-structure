a453907392107ffa57b3fca6414d34bdb71e3147
tommy <tommiful999@gmail.com>
2024-03-02 15:33:18 +0800

    fix common modules

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -11,6 +11,8 @@ const services = Object.values(providers);
   exports: services,
 })
 export class CommonModule implements NestModule {
+  
+  // Global Middleware
   public configure(consumer: MiddlewareConsumer): void {
     consumer.apply(LoggerContextMiddleware).forRoutes('*');
   }
